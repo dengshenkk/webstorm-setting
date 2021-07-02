2d64534d37114e6a1587433906f9035dabd8e313
haijinsha <dengshenkk@gmail.com>
2021-07-02 09:25:09 +0800

    WS-2021.1.2 <haijinsha@haijinsha.local Create baseRefactoring.xml, vim_settings.xml, Vue Single File Component.vue, ide.general.xml, _@user_One Dark Vivid Italic.icls, web-types-registry.xml, CodeGlance.xml, project.default.xml, git.xml, yiiguxing.translation.xml, postfixTemplates.xml, Default.xml, ignore.xml, find.xml, one_dark_config.xml, ui.lnf.xml, filetypes.xml, colors.scheme.xml, KeyPromoterXSettings.xml, templates.xml, emmet.xml, scss.scss, JavaScript.xml, docker.xml, laf.xml, web-browsers.xml, macros.xml, vcs.xml, grazie_global.xml, macOS.xml, keymap.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Vue Single File Component.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="${COMPONENT_NAME}-wrap">
+    #[[$END$]]#
+  </div>
+</template>
+
+<script>
+/**
+ * Created by ${USER} At ${YEAR}-${MONTH}-${DAY} ${TIME}:${SECOND}
+ */
+export default {
+  name: '${COMPONENT_NAME}',
+  props: {},
+  data() {
+    return {}
+  },
+  watch: {},
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      console.info('${COMPONENT_NAME} is init')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.${COMPONENT_NAME}-wrap {
+
+}
+</style>
